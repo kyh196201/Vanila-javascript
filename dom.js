883727bb3b338c5069cd8b0031a543bf8c474e79
kyh196201 <53055051+kyh196201@users.noreply.github.com>
2019-09-17 22:23:49 +0900

    #2-2 DOM Javascript
    Create Calculator Object

---
+++ dom.js
@@ -0,0 +1,10 @@
+const paragraph = document.querySelector("p");
+
+paragraph.innerHTML = "Hi my name is JS";
+
+function clickBtn() {
+  const button = document.getElementById("clickBtn");
+  button.style.color = "red";
+  button.style.backgroundColor = "white";
+  button.style.border = "none";
+}
