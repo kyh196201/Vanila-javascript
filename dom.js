946151691e4dbdefccf66090de8a8a992abc4cf5
kyh196201 <53055051+kyh196201@users.noreply.github.com>
2019-09-19 17:12:47 +0900

    #2-4 Event & Event Handler

--- dom.js
+++ dom.js
@@ -1,10 +1,26 @@
-const paragraph = document.querySelector("p");
+// const paragraph = document.querySelector("p");
 
-paragraph.innerHTML = "Hi my name is JS";
+// paragraph.innerHTML = "Hi my name is JS";
+// const button = document.getElementById("clickBtn");
+// function clickBtn() {
+//   button.style.color = "red";
+//   button.style.backgroundColor = "white";
+//   button.style.border = "none";
+//   button.style.padding = "22px";
+// }
 
-function clickBtn() {
-  const button = document.getElementById("clickBtn");
-  button.style.color = "red";
-  button.style.backgroundColor = "white";
-  button.style.border = "none";
+const title = document.querySelector("#title");
+
+function handleResize(event) {
+  console.log(event);
 }
+
+window.addEventListener("resize", handleResize);
+
+var target = document.querySelector("p");
+
+function handleClick() {
+  target.style.color = "red";
+}
+
+target.addEventListener("click", handleClick);
