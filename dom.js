5b30f6d1b146fba8cbcb76e685b7500932df72b4
kyh196201 <53055051+kyh196201@users.noreply.github.com>
2019-09-20 16:39:28 +0900

    #2-6 JS if, else practice one

--- dom.js
+++ dom.js
@@ -9,18 +9,29 @@
 //   button.style.padding = "22px";
 // }
 
+// function handleResize() {  //화면크기가 변경되는 이벤트
+//   console.log("Window Resized");
+// }
+
+// window.addEventListener("resize", handleResize);
+
 const title = document.querySelector("#title");
 
-function handleResize(event) {
-  console.log(event);
-}
-
-window.addEventListener("resize", handleResize);
-
-var target = document.querySelector("p");
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#9b59b6";
 
 function handleClick() {
-  target.style.color = "red";
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
 }
 
-target.addEventListener("click", handleClick);
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("click", handleClick);
+}
+
+init();
